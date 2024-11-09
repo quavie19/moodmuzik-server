05750baa32cfd3db2e10b171abe565455116857e
Jaquavia <97323413+quavie19@users.noreply.github.com>
2024-11-09 12:27:23 -0500

    domain name change

--- api/server.js
+++ api/server.js
@@ -27,7 +27,7 @@ app.use(
       name: 'MyCoolWebAppCookieName',
       secure: process.env.NODE_ENV === 'production', // true in production
       maxAge: 60 * 60 * 1000, // Session lasts for 1 hour (in milliseconds)
-      domain: '.domain.com',
+      domain: 'https://moodmuzik-server.onrender.com',
       sameSite: 'none',
     },
   })
